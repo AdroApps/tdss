d063045ad83b4e645b1c7fe334d6c5a3593a1ea3
webdeveloper1109 <webdeveloper1109@gmail.com>
2017-09-28 14:29:04 -0500

    changes for select dropdown of organisation

--- adminaddemployee.php
+++ adminaddemployee.php
@@ -3,14 +3,13 @@ include_once "conn.php";
 include_once "header.php";
 if(isset($_POST['name']))   {
 	$ename = $_POST['name'];
-	$pan = $_POST['number'];
-	$adhar = $_POST['number'];
-
-	$sql = "INSERT INTO `addemployee`(`employeename` , `pan` ,`adhar`,`userid`) VALUES ('".$ename."' , '".$pan."' , '".$adhar."','".$_SESSION['user_id']."')";  
+	$pan = $_POST['pan'];
+	$adhar = $_POST['adhar'];
+    $orgid=$_POST['orgid'];
+	$sql = "INSERT INTO `addemployee`(`employeename` , `pan` ,`adhar`,`userid`,`orgid`) VALUES ('".$ename."' , '".$pan."' , '".$adhar."','".$_SESSION['user_id']."','".$orgid."')";  
 	
 	$data  = mysqli_query($conn,$sql);
 	
-	echo $sql;
 	if($data)
 	{
 		echo 'moved database' ;
@@ -50,7 +49,7 @@ if(isset($_POST['name']))   {
 				<th>Employee Name</th>
 				<th>Pan Number</th>
 				<th>Adhar Number</th>
-				
+				<th>Organization Name</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -64,6 +63,8 @@ if(isset($_POST['name']))   {
 					"<td>".$row[3]."</td>";
 					echo
 					"<td>".$row[4]."</td>";
+					echo
+					"<td>".getOrganisation($row[5])."</td>";
 					echo"
 					  <td><class='text-center'>
 							<ul class='icons-list'>
@@ -105,16 +106,31 @@ if(isset($_POST['name']))   {
       <div class="modal-body">
         <form action="" method="POST">
 			<div class="form-group">
-				<input type="text" class ="form-control" id="pan" name="number" Placeholder="Enter Pan Number">
+				<input type="text" class ="form-control" id="pan" name="pan" Placeholder="Enter Pan Number">
 			</div>
 			<div class="form-group">
-				<input type="text" class="form-control" id="name" name="ename" Placeholder="Enter Employee FullName">
+				<input type="text" class="form-control" id="name" name="name" Placeholder="Enter Employee FullName">
 			</div>
 			<div class="form-group">
-				<input type="text" class ="form-control" id="adhar" name="number" Placeholder="Enter Adhar Number">
-			</div>
-			
-		<input type="submit" value="submit" class="btn btn-md  btnbg btn-primary">
+				<input type="text" class ="form-control" id="adhar" name="adhar" Placeholder="Enter Adhar Number">
+			</div><div class="form-group">
+			<select class="form-control" id="orgid" name="orgid">
+ <option selected="selected"> Select Client</option>
+<?php $sql_getorg="SELECT id,organization FROM usercreate";
+
+$res_getorg=mysqli_query($conn, $sql_getorg);
+
+
+ while($row_org = mysqli_fetch_array($res_getorg)){
+
+ ?>
+ <option value="<?php echo $row_org[0];?>"><?php echo $row_org[1];?></option>
+ 
+ <?php
+  }
+?>
+</select>	</div>
+		<input type="button" value="submit" id="edit-submit" class="btn btn-md  btnbg btn-primary">
 	</form>
 	</div>
 	</div>
@@ -123,5 +139,12 @@ if(isset($_POST['name']))   {
 </div>
 <!---modal--->
 
-
-
+<?php 
+function getOrganisation($id){
+$conn = mysqli_connect(DB_HOST, DB_UNAME, DB_PASSWORD, DB_NAME);
+ $getorg="SELECT organization FROM usercreate WHERE id=".$id;
+$rgetorg=mysqli_query($conn, $getorg);
+$row = mysqli_fetch_assoc($rgetorg);
+return $row['organization'];
+}
+?>
